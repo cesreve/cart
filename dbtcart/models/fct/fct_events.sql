7a141830ce01d5a33edadb21d73ab8565fe4dd8b
cesreve <cesreve@gmail.com>
2025-01-02 16:12:00 +0100

    Update fct_events.sql

--- dbtcart/models/fct/fct_events.sql
+++ dbtcart/models/fct/fct_events.sql
@@ -1,36 +1,3 @@
 select event_id, shopper_id, event_type , event_timestamp,
 	unnest(payload) 
 from {{ ref('src_events') }}
- 
- 
- 
- 
- 
- 
- 
- /*
- with step1 as (
-	select
-		event_id 
-		,shopper_id 
-		,event_type 
-		,event_timestamp 
-		,unnest(payload, recursive := true)
-	from {{ ref('src_events') }}
-)
-
-,products as (
-	select 
-		event_id
-		,unnest(products, recursive := true) as products
-	from step1
-)
-
-select 
-	step1.* exclude(products)
-	,p.* exclude(event_id)
-from step1
-left join products p
-	on step1.event_id = p.event_id
-order by event_timestamp
-*/
